4a54d0a5d1f0af920fbdabc585072e137896fb88
meysam <meisam.design.ph@gmail.com>
2022-03-03 14:55:31 +0330

    filter on bade category

--- src/components/TransActionComponent.js
+++ src/components/TransActionComponent.js
@@ -1,17 +1,55 @@
 import { useEffect, useState } from "react";
 
-const TransActionComponent = (props) => {
+const TransActionComponent = ({ transaction, onDelete }) => {
   const [searchItem, setSearchItem] = useState("");
-  const [filterTransaction, setFilterTransaction] = useState(props.transaction);
+  const [selectItem, setSelectItem] = useState("");
+  const [filterTransaction, setFilterTransaction] = useState(transaction);
+
+  const filterCategory = (cat) => {
+    switch (cat) {
+      case "All":
+        return setFilterTransaction(transaction);
+      case "Other": {
+        const filtered = transaction.filter((t) => t.category === "Other");
+        return setFilterTransaction(filtered);
+      }
+      case "Income": {
+        const filtered = transaction.filter((t) => t.category === "Income");
+        return setFilterTransaction(filtered);
+      }
+      case "Hire": {
+        const filtered = transaction.filter((t) => t.category === "Hire");
+        return setFilterTransaction(filtered);
+      }
+      case "Installment": {
+        const filtered = transaction.filter(
+          (t) => t.category === "Installment"
+        );
+        return setFilterTransaction(filtered);
+      }
+      case "Comestible": {
+        const filtered = transaction.filter((t) => t.category === "Comestible");
+        return setFilterTransaction(filtered);
+      }
+      case "Garment": {
+        const filtered = transaction.filter((t) => t.category === "Garment");
+        return setFilterTransaction(filtered);
+      }
+      default:
+        return setFilterTransaction(transaction);
+    }
+  };
 
   const filteredTrasaction = (search) => {
     if (!search || search === "") {
-      setFilterTransaction(props.transaction);
+      setFilterTransaction(transaction);
+      filterCategory(selectItem);
       return;
     }
-    const filtered = props.transaction.filter((t) =>
+    const filtered = transaction.filter((t) =>
       t.desc.toLowerCase().includes(search.toLowerCase())
     );
+    // filterCategory(selectItem);
     setFilterTransaction(filtered);
   };
 
@@ -20,22 +58,45 @@ const TransActionComponent = (props) => {
     filteredTrasaction(e.target.value);
   };
 
+  const categoryHandler = (e) => {
+    setSelectItem(e.target.value);
+    filterCategory(e.target.value);
+  };
+
   useEffect(() => {
     filteredTrasaction(searchItem);
-  }, [props.transaction]);
+  }, [transaction]);
 
-  if (!props.transaction.length)
+  if (!transaction.length)
     return <h3 className="title">Add some transactions</h3>;
 
   return (
-    <section>
-      <input
-        className="search"
-        type="text"
-        value={searchItem}
-        onChange={changeHandler}
-        placeholder="search for transactions"
-      />
+    <section className="transactionBox">
+      <div className="filter">
+        <input
+          className="search"
+          type="text"
+          value={searchItem}
+          onChange={changeHandler}
+          placeholder="search for transactions"
+        />
+
+        <select
+          name="category"
+          value={selectItem}
+          onChange={categoryHandler}
+          placeholder="category"
+        >
+          <option>All</option>
+          <option>Other</option>
+          <option>Income</option>
+          <option>Hire</option>
+          <option>Installment</option>
+          <option>Comestible</option>
+          <option>Garment</option>
+        </select>
+      </div>
+
       {filterTransaction.length
         ? filterTransaction.map((t) => {
             return (
@@ -47,7 +108,7 @@ const TransActionComponent = (props) => {
                 <span className="desc">{t.desc}</span>
                 <span>$ {t.amount}</span>
                 <span>{t.category}</span>
-                <button className="delete" onClick={() => props.onDelete(t.id)}>
+                <button className="delete" onClick={() => onDelete(t.id)}>
                   Delete
                 </button>
               </div>
